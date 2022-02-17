7c83a0ac41d1b9243541a7a51388b681212c30cb
Weijing Jay Lin <weijingjaylin@gmail.com>
2022-02-17 13:59:13 -0800

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const CommonStyles = StyleSheet.create({
+  titleText: {
+    fontSize: 20,
+  },
+  centerView: {
+    alignItems: "center",
+  },
+  view20p: {
+    width: "20%",
+  },
+});
